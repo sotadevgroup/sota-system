426f563a863e958d041553e7c67c76b1bf324e24
Lewis Bird <lewis@sotamedia.co.uk>
2018-09-26 13:06:33 +0100

    auth routes

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Sota\System\Controllers\AuthController;
+
+Route::middleware('api')
+    ->prefix('api/auth')
+    ->group(function () {
+
+        Route::post('login', '\\'.AuthController::class.'@login');
+        Route::post('logout', '\\'.AuthController::class.'@logout');
+        Route::post('refresh', '\\'.AuthController::class.'@refresh');
+        Route::post('user', '\\'.AuthController::class.'@user');
+
+    }
+);
 
 Route::get('{any}', function ($any = '') {
     return response()
